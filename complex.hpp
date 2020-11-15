7a9491aabe249150f234fff3a6225b8dc98d9631
NobuyukiIMAI <nobushi1121@gmail.com>
2020-11-15 22:33:48 +0900

    Update

--- complex.hpp
+++ complex.hpp
@@ -1,18 +1,89 @@
+#include <iostream>
 #include <string>
+
+#define MY_DEBUG
 
 using namespace std;
 
 class Complex {
 private:
-    int _img, _real;
+    int _real, _imag;
 
 public:
-    int img() { return _img; }
+    Complex(int real, int imag) : _real(real), _imag(imag) {
+        #ifdef MY_DEBUG
+        cout << to_string(_real) + " + " + to_string(_imag) + "i" << endl;
+        #endif
+    }
+
+    // 二項演算子で整数のみでコンストラクタが走るようにするため
+    Complex(int real) : _real(real), _imag(0) {
+        #ifdef MY_DEBUG
+        cout << to_string(_real) + " + " + to_string(_imag) + "i" << endl;
+        #endif
+    }
+
+    // コピーコンストラクタ
+    Complex(const Complex &c) {
+        *this = c;
+    }
+
+    // デストラクタは、メモリ領域を確保した場合に明示的に書く
+    // デストラクタを記述しなければ、デフォルトデストラクタをコンパイラが生成する
+    // 何もしないデストラクタを記述すると（？）、違反命令発行で落ちる
+    // -> デフォルトデストラクタがかぶって悪さする？
+    // ~Complex() {
+    //     #ifdef MY_DEBUG
+    //     cout << to_string(_real) + " + " + to_string(_imag) + "i" << endl;
+    //     #endif
+    // }
+
+    Complex &operator=(const Complex &c) {
+        _real = c._real;
+        _imag = c._imag;
+        return *this;
+    }
+
+    Complex &operator+=(const Complex &c) {
+        _real += c._real;
+        _imag += c._imag;
+        return *this;
+    }
+
+    // 引数が実数（整数）の場合はコンストラクタが走らなくなる
+    // Complex(int real) を定義しているのに、必要なのか？
+    Complex &operator+=(const int real) {
+        _real += real;
+        return *this;
+    }
+
+    Complex &operator-=(const Complex &c) {
+        _real -= c._real;
+        _imag -= c._imag;
+        return *this;
+    }
+
+public:
     int real() { return _real; }
-    string str() { return to_string(_real) + " + " + to_string(_img) + "i"; }
+    int imag() { return _imag; }
+    string str() { return to_string(_real) + " + " + to_string(_imag) + "i"; }
 
 };
 
+// 二項演算子で、Complex(int real)があるので、どちらかが整数でもコンストラクタが走る
+
 Complex operator+(const Complex &c1, const Complex &c2) {
+    Complex ret = c1;
+    ret += c2;
+    return ret;
+}
+
+Complex operator-(const Complex &c1, const Complex &c2) {
+    Complex ret = c1;
+    ret -= c2;
+    return ret;
+}
+
+Complex operator*(const Complex &c1, const Complex &c2) {
 
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include "complex.hpp"
-#include <iostream>
 
 using namespace std;
 
 int main() {
     Complex i(0, 1);
-    Complex a = 1 + 2 * i;
-    Complex b = 3 + 4 * i;
-    Complex c = a + b;
-    Complex d = a * b;
-    Complex e = a - 1;
-    Complex f = a - 2 * i;
+    i += 1;
+    cout << "i = " << i.str() << endl;
     
-    cout << "a = " << a.str() << endl;
-    cout << "b = " << b.str() << endl;
-    cout << "c = " << c.str() << endl;
-    cout << "d = " << d.str() << endl;
-    cout << "e = " << e.str() << endl;
-    cout << "f = " << f.str() << endl;
+    // Complex a = 1 + 2 * i;
+    // Complex b = 3 + 4 * i;
+    // Complex c = a + b;
+    // Complex d = a * b;
+    // Complex e = a - 1;
+    // Complex f = a - 2 * i;
+
+    // cout << "a = " << a.str() << endl;
+    // cout << "b = " << b.str() << endl;
+    // cout << "c = " << c.str() << endl;
+    // cout << "d = " << d.str() << endl;
+    // cout << "e = " << e.str() << endl;
+    // cout << "f = " << f.str() << endl;
 }
